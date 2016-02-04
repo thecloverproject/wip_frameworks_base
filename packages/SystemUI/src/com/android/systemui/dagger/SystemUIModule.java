c5249bb6241181a891cb8ce5bc9c42d9a6184c7c
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Co-authored-by: chillger <alexander.loskutov@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -51,6 +51,7 @@ import com.android.systemui.bouncer.domain.interactor.BouncerInteractorModule;
 import com.android.systemui.bouncer.ui.BouncerViewModule;
 import com.android.systemui.classifier.FalsingModule;
 import com.android.systemui.clipboardoverlay.dagger.ClipboardOverlayModule;
+import com.android.systemui.clover.CloverModule;
 import com.android.systemui.common.data.CommonDataLayerModule;
 import com.android.systemui.common.ui.ConfigurationModule;
 import com.android.systemui.common.ui.data.repository.ConfigurationRepositoryModule;
@@ -226,6 +227,7 @@ import javax.inject.Named;
         CameraProtectionModule.class,
         ClipboardOverlayModule.class,
         ClockRegistryModule.class,
+        CloverModule.class,
         CommunalModule.class,
         CommonDataLayerModule.class,
         ComposeModule.class,
