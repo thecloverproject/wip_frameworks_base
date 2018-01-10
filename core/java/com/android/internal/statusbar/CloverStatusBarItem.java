17efcccd46ac52cb762bf00d964968f501b994dc
Sam Mortimer <sam@mortimer.me.uk>
2018-01-09 22:16:48 -0800

    SystemUI: Add Clover statusbar item holder
    *) To be used as a layout to hold one or more lineage-sdk status bar
       feature classes.
    
    *) Propagates visibility and tint information via interfaces
       defined in lineage-sdk internal.
    
    *) Supports adding further interfaces without impacting existing use
       cases.
    
    Co-authored-by: grepfox <grepfox@tutamail.com>
    Change-Id: I4d4361fcb682c1940133bee1029b8b759f93e05f

---
+++ core/java/com/android/internal/statusbar/CloverStatusBarItem.java
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.statusbar;
+
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewParent;
+
+import java.util.ArrayList;
+
+public class CloverStatusBarItem {
+
+    public interface Manager {
+        void addDarkReceiver(DarkReceiver darkReceiver);
+        void removeDarkReceiver(DarkReceiver darkReceiver);
+        void addVisibilityReceiver(VisibilityReceiver visibilityReceiver);
+        void removeVisibilityReceiver(VisibilityReceiver visibilityReceiver);
+    }
+
+    public interface DarkReceiver {
+        void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint);
+        void setFillColors(int darkColor, int lightColor);
+    }
+
+    public interface VisibilityReceiver {
+        void onVisibilityChanged(boolean isVisible);
+    }
+
+    // Locate parent CloverStatusBarItem.Manager
+    public static Manager findManager(View v) {
+        ViewParent parent = v.getParent();
+        if (parent == null) {
+            return null;
+        } else if (parent instanceof Manager) {
+            return (Manager) parent;
+        } else if (parent instanceof View) {
+            return findManager((View) parent);
+        } else {
+            return null;
+        }
+    }
+}
