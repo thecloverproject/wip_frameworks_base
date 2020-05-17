726d376007e8d12fe97f72e5d4f65b2392f18ea8
Jesse Chan <jc@lineageos.org>
2020-05-17 22:41:42 +0800

    Implement click to take partial screenshot [1/2]
    Default to the old long press behaviour when the feature is enabled.
    
    Change-Id: I5fc37f818b06db2bd2d315cffee2041a740781ed
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: RiteshSahany <ritesh@thecloverproject.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -289,5 +289,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.TORCH_LONG_PRESS_POWER_GESTURE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.TORCH_LONG_PRESS_POWER_TIMEOUT, new InclusiveIntegerRangeValidator(0, 3600));
         VALIDATORS.put(System.AUTO_BRIGHTNESS_ONE_SHOT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.CLICK_PARTIAL_SCREENSHOT, BOOLEAN_VALIDATOR);
     }
 }
