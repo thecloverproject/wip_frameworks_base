HaloUI-AOSP/android_frameworks_base

SystemUI: Network Traffic
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Wolfram Liebchen <achalmgucker@googlemail.com>
Co-authored-by: Patrick Gaskin <patrick@pgaskin.net>
Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -25,6 +25,8 @@ import android.view.View;
 import com.android.systemui.res.R;
 import com.android.systemui.shared.statusbar.phone.BarTransitions;
 
+import org.lineageos.internal.statusbar.NetworkTraffic;
+
 public final class PhoneStatusBarTransitions extends BarTransitions {
     private static final float ICON_ALPHA_WHEN_NOT_OPAQUE = 1;
     private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK = 0.5f;
@@ -35,6 +37,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private boolean mIsHeadsUp;
 
     private View mStartSide, mStatusIcons, mBattery;
+    private NetworkTraffic mNetworkTrafficStart, mNetworkTrafficCenter, mNetworkTrafficEnd;
     private Animator mCurrentAnimation;
 
     /**
@@ -46,7 +49,13 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mIconAlphaWhenOpaque = res.getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
         mStartSide = statusBarView.findViewById(R.id.status_bar_start_side_except_heads_up);
         mStatusIcons = statusBarView.findViewById(R.id.statusIcons);
+        mNetworkTrafficStart = statusBarView.findViewById(R.id.network_traffic_start);
+        mNetworkTrafficCenter = statusBarView.findViewById(R.id.network_traffic_center);
+        mNetworkTrafficEnd = statusBarView.findViewById(R.id.network_traffic_end);
         mBattery = statusBarView.findViewById(R.id.battery);
+        mNetworkTrafficStart.setViewPosition(0);        /* start side display */
+        mNetworkTrafficCenter.setViewPosition(1);       /* center display */
+        mNetworkTrafficEnd.setViewPosition(2);          /* end side display */
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -115,6 +124,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.playTogether(
                     animateTransitionTo(mStartSide, newStartSideAlpha),
                     animateTransitionTo(mStatusIcons, newStatusIconsAlpha),
+                    animateTransitionTo(mNetworkTrafficStart, newStatusIconsAlpha),
+                    animateTransitionTo(mNetworkTrafficCenter, newStatusIconsAlpha),
+                    animateTransitionTo(mNetworkTrafficEnd, newStatusIconsAlpha),
                     animateTransitionTo(mBattery, newBatteryAlpha)
                     );
             if (isLightsOut(mode)) {
@@ -125,6 +137,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         } else {
             mStartSide.setAlpha(newStartSideAlpha);
             mStatusIcons.setAlpha(newStatusIconsAlpha);
+            mNetworkTrafficStart.setAlpha(newStatusIconsAlpha);
+            mNetworkTrafficCenter.setAlpha(newStatusIconsAlpha);
+            mNetworkTrafficEnd.setAlpha(newStatusIconsAlpha);
             mBattery.setAlpha(newBatteryAlpha);
         }
     }

thecloverproject/wip_frameworks_base

Co-authored-by: Bruno Martins <bgcngm@gmail.com>
Signed-off-by: RiteshSahany <ritesh@thecloverproject.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -25,6 +25,8 @@ import android.view.View;
 import com.android.systemui.res.R;
 import com.android.systemui.shared.statusbar.phone.BarTransitions;
 
+import com.android.internal.statusbar.NetworkTraffic;
+
 public final class PhoneStatusBarTransitions extends BarTransitions {
     private static final float ICON_ALPHA_WHEN_NOT_OPAQUE = 1;
     private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK = 0.5f;
@@ -35,6 +37,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private boolean mIsHeadsUp;
 
     private View mStartSide, mStatusIcons;
+    private NetworkTraffic mNetworkTrafficStart, mNetworkTrafficCenter, mNetworkTrafficEnd;
     private Animator mCurrentAnimation;
 
     /**
@@ -46,6 +49,12 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mIconAlphaWhenOpaque = res.getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
         mStartSide = statusBarView.findViewById(R.id.status_bar_start_side_except_heads_up);
         mStatusIcons = statusBarView.findViewById(R.id.statusIcons);
+        mNetworkTrafficStart = statusBarView.findViewById(R.id.network_traffic_start);
+        mNetworkTrafficCenter = statusBarView.findViewById(R.id.network_traffic_center);
+        mNetworkTrafficEnd = statusBarView.findViewById(R.id.network_traffic_end);
+        mNetworkTrafficStart.setViewPosition(0);        /* start side display */
+        mNetworkTrafficCenter.setViewPosition(1);       /* center display */
+        mNetworkTrafficEnd.setViewPosition(2);          /* end side display */
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -112,7 +121,10 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             AnimatorSet anims = new AnimatorSet();
             anims.playTogether(
                     animateTransitionTo(mStartSide, newStartSideAlpha),
-                    animateTransitionTo(mStatusIcons, newStatusIconsAlpha)
+                    animateTransitionTo(mStatusIcons, newStatusIconsAlpha),
+                    animateTransitionTo(mNetworkTrafficStart, newStatusIconsAlpha),
+                    animateTransitionTo(mNetworkTrafficCenter, newStatusIconsAlpha),
+                    animateTransitionTo(mNetworkTrafficEnd, newStatusIconsAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -122,6 +134,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         } else {
             mStartSide.setAlpha(newStartSideAlpha);
             mStatusIcons.setAlpha(newStatusIconsAlpha);
+            mNetworkTrafficStart.setAlpha(newStatusIconsAlpha);
+            mNetworkTrafficCenter.setAlpha(newStatusIconsAlpha);
+            mNetworkTrafficEnd.setAlpha(newStatusIconsAlpha);
         }
     }
 }